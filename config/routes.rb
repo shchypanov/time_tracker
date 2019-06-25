6308f5e674d408ddd31924a9c28b177aa7be8677
Nkotelevets <Nkotelevets1996@gmail.com>
2019-06-25 14:39:35 +0300

    new admin page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/profile', to: 'application#profile'
+  get '/admin', to: 'application#admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
 end
