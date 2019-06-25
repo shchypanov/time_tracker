f01cf5cd05bcff778c7e1473cbcd1572208455ed
Vlad <shchipanovv@gmail.com>
2019-06-25 21:22:11 +0300

    strange registration

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get '/profile', to: 'application#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
+
+  devise_scope :user do
+    get "signup", to: "devise/registrations#new"
+    get "login", to: "devise/sessions#new"
+    get "logout", to: "devise/sessions#destroy"
+  end
 end
