2d508552559c3e50a430ab33fba9b7563b7a0d5f
ArthurZakharchenko <manclassic2@gmail.com>
2019-06-25 17:05:37 +0300

    add btn

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   get '/profile', to: 'application#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
+
+  get '/my_tasks', to: 'application#my_tasks'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
