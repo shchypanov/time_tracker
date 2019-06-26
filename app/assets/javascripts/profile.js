895b1c068c690964ebb4eb50f938de7d825fa8b6
evilentE <epihina3001@gmail.com>
2019-06-26 19:16:24 +0300

    hid button AddTask

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,12 +1,15 @@
 function contLoaded() {
   var buttonAddTask = document.getElementById("profile_button-addTask");
-  buttonAddTask.addEventListener('click', addTaskClick);
-  console.log(buttonAddTask);
+/*  buttonAddTask.addEventListener('click', addTaskClick);
+  console.log(buttonAddTask);*/
+   buttonAddTask.onclick = function() {
+   document.getElementById('profile_container-second').style.display = 'block';
+ }
 }
-
+/*
 function addTaskClick(evt) {
 	console.log(evt)
 }
-
+*/
 document.addEventListener('DOMContentLoaded', contLoaded);
 
