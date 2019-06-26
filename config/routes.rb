0af92e3279ab474675dd0ea4e889ae20a8d7d39e
Vlad <shchipanovv@gmail.com>
2019-06-26 14:15:38 +0300

    chenged registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   get '/profile', to: 'application#profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#home'
-
   devise_scope :user do
-    get "signup", to: "devise/registrations#new"
-    get "login", to: "devise/sessions#new"
-    get "logout", to: "devise/sessions#destroy"
+    authenticated  do
+      root to: 'application#profile'
+    end
+
+    unauthenticated do
+      root to: 'devise/sessions#new', as: 'unauthenticated_root'
+    end
   end
-end
+  end
