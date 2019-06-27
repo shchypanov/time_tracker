2fe5e4893cd7f7646056e7b7829217085763a273
Vlad <shchipanovv@gmail.com>
2019-06-27 19:02:35 +0300

    check admin?

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def admin?
+    role == 'admin'
+  end
+  
 end
