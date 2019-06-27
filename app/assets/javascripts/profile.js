d161147ff25d3596072dbb1f14b8edc5d6e8b83c
evilentE <epihina3001@gmail.com>
2019-06-27 17:13:44 +0300

    hide stop and pause button

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -5,21 +5,13 @@ function contLoaded() {
   buttonAddTask.onclick = function() {
     document.getElementById('profile_container-second').style.display = 'block';
     buttonAddTask.onclick = function() {
+      document.getElementById('profile_button-pause').style.display = 'none';
+      document.getElementById('profile_button-stop').style.display = 'none';
       var blockClient = document.getElementById('profile_container-second');
       var allBlock = blockClient.querySelector('.profile_block-client');
       var clone = allBlock.cloneNode(true);
       blockClient.appendChild(clone);
-      /*var selectClient = document.getElementById('profile_select-client__select');
-      console.log(123)
-      console.log(selectClient);
-      var newSelectClient = document.createElement("select");
-      selectClient.appendChild(newSelectClient);
-      console.log(newSelectClient);*/
     }
   }
 }
-/*  function addTaskClick(evt) {
-  	console.log(evt)
-  }
-*/
 document.addEventListener('DOMContentLoaded', contLoaded);
