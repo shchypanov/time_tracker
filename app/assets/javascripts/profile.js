4cc1a1ac86f8d349b1fd89639d1a05263b729db3
evilentE <epihina3001@gmail.com>
2019-06-28 13:21:10 +0300

    all pause and stop buttons are hidden when push

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,15 +1,27 @@
 function contLoaded() {
   var buttonAddTask = document.getElementById("profile_button-addTask");
+
   buttonAddTask.onclick = function() {
-    document.getElementById('profile_container-second').style.display = 'block';
-    buttonAddTask.onclick = function() {
+    if (document.getElementById('profile_container-second').style.display == 'block') {
       document.getElementById('profile_button-pause').style.display = 'none';
       document.getElementById('profile_button-stop').style.display = 'none';
       var blockClient = document.getElementById('profile_container-second');
       var allBlock = blockClient.querySelector('.profile_block-client');
       var clone = allBlock.cloneNode(true);
       blockClient.appendChild(clone);
+    } else {
+      document.getElementById('profile_container-second').style.display = 'block';
+    }
+
+/*    var buttonStart = document.getElementById('profile_button-start');
+    buttonStart.onclick = function() {
+      var buttonPause = document.getElementById('profile_button-pause');
+      var buttonStop = document.getElementById('profile_button-stop');
+
+      buttonStart.style.display = "none";
+      buttonPause.style.display = "initial";
+      buttonStop.style.display = "initial";*/
     }
   }
-}
+
 document.addEventListener('DOMContentLoaded', contLoaded);
