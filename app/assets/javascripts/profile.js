966d65a72db7cb68f34e671f09311c49da68d1ab
evilentE <epihina3001@gmail.com>
2019-06-28 19:33:23 +0300

    pri nazhatii addaks. vypadayut taski

--- app/assets/javascripts/profile.js
+++ app/assets/javascripts/profile.js
@@ -1,27 +1,22 @@
-function contLoaded() {
+document.addEventListener('DOMContentLoaded', function() {
+
   var buttonAddTask = document.getElementById("profile_button-addTask");
 
   buttonAddTask.onclick = function() {
-    if (document.getElementById('profile_container-second').style.display == 'block') {
-      document.getElementById('profile_button-pause').style.display = 'none';
-      document.getElementById('profile_button-stop').style.display = 'none';
-      var blockClient = document.getElementById('profile_container-second');
-      var allBlock = blockClient.querySelector('.profile_block-client');
-      var clone = allBlock.cloneNode(true);
-      blockClient.appendChild(clone);
-    } else {
-      document.getElementById('profile_container-second').style.display = 'block';
-    }
+    var taskBlock = document.querySelector('#profile_container-second .profile_block-client');
 
-/*    var buttonStart = document.getElementById('profile_button-start');
-    buttonStart.onclick = function() {
-      var buttonPause = document.getElementById('profile_button-pause');
-      var buttonStop = document.getElementById('profile_button-stop');
+    var newForm = document.createElement('form');
+    newForm.innerHTML = (
+      '<select class="profile_select-client__input" id="profile_select-client__select" name="project_id">' +
+      '<option class="profile_select-client__option">Task 1</option>' +
+      '<option class="profile_select-client__option">Task 2</option>' +
+      '</select>' +
+      '<input type="text" class="profile_select-client__input" placeholder="Task description" id="profile_select-client__input" name="description" required>' +
+      '<button class="profile_button profile_button-start" type="button" id="profile_button-start">Start</button>' +
+      '<button class="profile_button profile_button-pause" type="button" id="profile_button-pause">Pause</button>' +
+      '<button class="profile_button profile_button-stop" type="button" id="profile_button-stop">Stop</button>'
+    );
 
-      buttonStart.style.display = "none";
-      buttonPause.style.display = "initial";
-      buttonStop.style.display = "initial";*/
-    }
+    taskBlock.appendChild(newForm);
   }
-
-document.addEventListener('DOMContentLoaded', contLoaded);
+});
