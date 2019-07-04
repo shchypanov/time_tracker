506c0d1b1eacfc7c570de059834635abf6f22e3e
Vlad <shchipanovv@gmail.com>
2019-07-04 20:28:46 +0300

    made destroy method, without frontend

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,11 +2,27 @@ class CustomersController < ApplicationController
 
   def create
     @customer = Customer.new(customer_params)
-    respond_to do |format| 
+    respond_to do |format|
       if @customer.name.present? && @customer.save
         format.js {render :created}
       else
         format.js {render :created_error}
+      end
+    end
+  end
+
+  # def delete
+  #   Customer.find(params[:id]).destroy
+  #   redirect_to admin_url
+  # end
+
+  def delete
+    @customer.destroy
+    respond_to do |format|
+      if @customer.destroy
+        format.js {render :destroy}
+      else
+        format.js {render :destroy_error}
       end
     end
   end
