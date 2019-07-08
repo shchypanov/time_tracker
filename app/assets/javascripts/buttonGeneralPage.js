ed50ea9890ced4d30a26caee99ff7b9746464017
evilentE <epihina3001@gmail.com>
2019-07-08 23:45:13 +0300

    before changing the start buttons

--- app/assets/javascripts/buttonGeneralPage.js
+++ app/assets/javascripts/buttonGeneralPage.js
@@ -1,24 +1,26 @@
-window.addEventListener('DOMContentLoaded', function(){
+window.addEventListener('DOMContentLoaded', function() {
 
-  var buttonStart = document.getElementById('profile_button-start');
-  var buttonPause = document.getElementById('profile_button-pause');
-  var buttonStop = document.getElementById('profile_button-stop');
-  
+  var buttonStart = document.getElementsByClassName('profile_button-start');
+  var buttonPause = document.getElementsByClassName('profile_button-pause');
+  var buttonStop = document.getElementsByClassName('profile_button-stop');
+
   buttonStart.onclick = pushButtonStart;
   buttonPause.onclick = pushButtonPause;
   buttonStop.onclick = pushButtonStop;
-  
-  function pushButtonStart(event){
+
+  function pushButtonStart(event) {
     buttonPause.style.display = "inline-block";
     buttonStop.style.display = "inline-block";
     buttonStart.style.display = "none";
   }
-  function pushButtonPause(event){
+
+  function pushButtonPause(event) {
     buttonPause.style.display = "none";
     buttonStop.style.display = "inline-block";
     buttonStart.style.display = "inline-block";
   }
-  function pushButtonStop(event){
+
+  function pushButtonStop(event) {
     buttonPause.style.display = "none";
     buttonStop.style.display = "none";
     buttonStart.style.display = "inline-block";
