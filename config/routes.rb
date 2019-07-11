a3f3de1e82219a41754aa01add3f2ffdaa456965
ekandreeva <e.andreeva0304@gmail.com>
2019-07-11 17:04:34 +0300

    add controller users

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,10 @@ Rails.application.routes.draw do
   get '/profile', to: 'application#profile'
   get '/admin', to: 'application#admin'
   get '/my_tasks', to: 'application#my_tasks'
+  # devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_scope :user do
+    get 'sign_up', to: 'users/registrations#new'
     authenticated  do
       root to: 'application#profile'
     end
